1fc33016f32f617b0e136239c2e89eeb7f12fc1f
renaldimramadhan <renaldimramadhan@gmail.com>
2022-09-05 22:26:01 +0700

    first commit

---
+++ src/components/Tugas.jsx
@@ -0,0 +1,16 @@
+import './Tugas.css';
+
+const TugasTypography = () => {
+  return (
+    <section class='Tugas'>
+      <h1> wle</h1>
+      <h2> wle wle </h2>
+      <h3> wle wle wle</h3>
+      <h4> wle wle wle wle</h4>
+      <h5> wle wle wle wle wle</h5>
+      <h6> wle wle wle wle wle wle</h6>
+    </section>
+  );
+};
+
+export default TugasTypography;
